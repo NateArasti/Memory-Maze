f850c339f89208e9659a0eef536ae67e265186ad
ncrmncr <stremousov02@mail.ru>
2021-06-02 03:29:19 +0500

    WOW WHATS THAT??????????????? ITS A TUTORIAL AAAAAAAAAAAAAAAAAAAAAAAAA OMGGG WTF!!!????!?!?!? HOW IS IT POSSIBLE?????????????????????????????? THERE IS A STORY SCROLLING FIX TOO OOOOOOH THATS IMPOSSIBLE!!!!!!!!!!!!!!! I KNOW THANK U

--- Memory Maze/Assets/GeneralScripts/MazeLoader.cs
+++ Memory Maze/Assets/GeneralScripts/MazeLoader.cs
@@ -4,75 +4,89 @@ using UnityEngine.SceneManagement;
 
 public enum GameMode
 {
-    Classic,
-    Arcade
+	Classic,
+	Arcade
 }
 
 public enum Difficulty
 {
-    Easy,
-    Normal,
-    Hard
+	Easy,
+	Normal,
+	Hard
 }
 
 public class MazeLoader : MonoBehaviour
 {
 #pragma warning disable 649
-    [SerializeField] private GameObject loadingScreen;
+	[SerializeField] private GameObject loadingScreen;
 #pragma warning restore 649
 
-    private void LoadMazeScene()
-    {
-        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Menu"))
-            ChangeScene.SwitchToScene("Maze");
-        else
-            SceneManager.LoadScene("Maze");
-    }
+	private void LoadMazeScene()
+	{
+		if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Menu"))
+			ChangeScene.SwitchToScene("Maze");
+		else
+			SceneManager.LoadScene("Maze");
+	}
 
-    private void LoadMenuScene() => SceneManager.LoadScene("Menu");
+	private void LoadMenuScene()
+	{
+		SceneManager.LoadScene("Menu");
+	}
 
-    private static GameMode _mode;
-    private static Difficulty Difficulty => ArcadeProgression.CurrentDifficulty;
-    
-    public void Awake() => Time.timeScale = 1;
-    
-    public void SetGameMode(int newMode)
-    {
-        _mode = (GameMode)newMode;
-    }
-    
-    public void SetDifficulty(int newDifficulty)
-    {
-        ArcadeProgression.CurrentDifficulty = (Difficulty)newDifficulty;
-    }
+	private static GameMode _mode;
 
-    public void LoadMaze()
-    {
-        switch (_mode)
-        {
-            case GameMode.Arcade:
-                ArcadeProgression.MoveToNextProgressionLevel();
-                LoadMazeScene();
-                break;
-            case GameMode.Classic:
-                LoadMazeScene();
-                break;
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
-    }
+	public static bool IsTutorial { get; private set; }
+	private static Difficulty Difficulty => ArcadeProgression.CurrentDifficulty;
 
-    public void ExitMaze()
-    {
-        if(ArcadeProgression.ProgressionOn) ArcadeProgression.Dispose();
-        LoadMenuScene();
-    }
+	public void Awake()
+	{
+		Time.timeScale = 1;
+	}
 
-    private void RestartArcadeMode(Difficulty difficulty)
-    {
-        SetDifficulty((int)difficulty);
-        ArcadeProgression.Dispose();
-        ArcadeProgression.MoveToNextProgressionLevel();
-        LoadMazeScene();
-    }
+	public void SetGameMode(int newMode)
+	{
+		_mode = (GameMode) newMode;
+	}
+
+	public void SetDifficulty(int newDifficulty)
+	{
+		ArcadeProgression.CurrentDifficulty = (Difficulty) newDifficulty;
+	}
+
+	public void LoadMaze()
+	{
+		switch (_mode)
+		{
+			case GameMode.Arcade:
+				ArcadeProgression.MoveToNextProgressionLevel();
+				LoadMazeScene();
+				break;
+			case GameMode.Classic:
+				LoadMazeScene();
+				break;
+			default:
+				throw new ArgumentOutOfRangeException();
+		}
+	}
+
+	public void ExitMaze()
+	{
+		if (ArcadeProgression.ProgressionOn) ArcadeProgression.Dispose();
+		LoadMenuScene();
+	}
+
+	private void RestartArcadeMode(Difficulty difficulty)
+	{
+		SetDifficulty((int) difficulty);
+		ArcadeProgression.Dispose();
+		ArcadeProgression.MoveToNextProgressionLevel();
+		LoadMazeScene();
+	}
+
+	public void LoadTutorialMaze()
+	{
+		SceneManager.LoadScene("Maze");
+		IsTutorial = true;
+	}
 }
